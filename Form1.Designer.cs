b0991f2b9f4c06bf6ce7d0ed9595720176dcde1c
404oops <51266541+404oops@users.noreply.github.com>
2021-12-27 19:01:08 +0100

    Zavrsen projekat

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Majmuni
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(148, 133);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Pocni";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(24, 133);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(6);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(103, 29);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Tacno";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Klikni na \"Pocni\"";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Majmuni.Properties.Resources.majmun;
+            this.pictureBox1.Location = new System.Drawing.Point(430, 17);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(434, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(148, 279);
+            this.button2.Margin = new System.Windows.Forms.Padding(6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 44);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 248);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Poeni idu ovde!";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 46);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label3.Size = new System.Drawing.Size(283, 50);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Klikni na Tacno ako je tacno\r\nA ako nije onda ignorisi";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 342);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Windows.Forms;
+namespace Majmuni
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int pitanje, poeni;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "Sledece";
+            button2.Text = "Proveri";
+            label3.Text = null;
+            PrikaziSve();
+            pitanje += 1;
+            radioButton1.Checked = false;
+            switch (pitanje)
+            {
+                case 1:
+                    label1.Text = "Majmuni vole cokoadne bananice"; // Pitanja je smisljao Ognjen
+                    break;
+                case 2:
+                    label1.Text = "Majmuni najvise vole da se penju na drvo";
+                    break;
+                case 3:
+                    label1.Text = "Na slici je Veliki Majmun";
+                    break;
+                case 4:
+                    label1.Text = "Ova vrsta je izumrla";
+                    break;
+                case 5:
+                    label1.Text = "Gotovi smo!";
+                    label3.Text = "Ukupni Poeni: " + poeni;
+                    SakrijSve();
+                    break;
+            }
+        }
+
+        private void PrikaziSve() // Ideju smisljao Nikola
+        {
+            label1.Visible = true;
+            label2.Visible = true;
+            radioButton1.Visible = true;
+            button2.Visible = true;
+            button1.Left = 12;
+            button1.Top = 145;
+        }
+        private void SakrijSve()
+        {
+            label2.Visible = false;
+            radioButton1.Visible = false;
+            button2.Visible = false;
+            button1.Visible = false; 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            switch (pitanje)
+            {
+                case 1:
+                    if (radioButton1.Checked == false)
+                    {
+                        gotovZadatak();
+                    }
+                    break;
+                case 2:
+                    if (radioButton1.Checked)
+                    {
+                        gotovZadatak();
+                    }
+                    break;
+                case 3:
+                    if (radioButton1.Checked)
+                    {
+                        gotovZadatak(20);
+                    }
+                    break;
+                case 4:
+                    if (radioButton1.Checked)
+                    {
+                        gotovZadatak(40); // Poene i ovaj sistem je smisljao Marko
+                    }
+                    break;
+            }
+        }
+
+        private void gotovZadatak(int n = 10) // Marko
+        {
+            poeni += n;
+            label2.Text = "Poeni: " + poeni;
+        }
+    }
+}
